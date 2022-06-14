7d4e625f8e9af118d83024488fa44ff70119272d
pq707 <101611731+pq707@users.noreply.github.com>
2022-06-14 10:22:40 +0700

    Add files via upload

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+<h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Rifky</li>
+ <li>Nama Belakang : ganteng</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan  Raya.</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Depok keras</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>5 jari</li>
+ <li>pushup</li>
+ </ul>
+</section>
---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+<footer>
+ <a href="http://www.RentalBuku.com">RentalPees</a>
+ </footer>
+ </div>
+</body>
+</html>
